8381d14129cd81b97cfbedf841590d3f5afcf7ce
shuvo8453 <saifulshuvo2@gmail.com>
2023-06-02 03:25:17 +0600

    remove comments

--- resources/views/student/pages/feedback/show.blade.php
+++ resources/views/student/pages/feedback/show.blade.php
@@ -4,16 +4,6 @@
 @endsection
 
 @section('content')
-{{--    <form>--}}
-{{--        <div class="form-group">--}}
-{{--            <label for="exampleFormControlInput1"> Email address</label>--}}
-{{--            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">--}}
-{{--        </div>--}}
-{{--        <div class="form-group">--}}
-{{--            <label for="exampleFormControlTextarea1">Example textarea</label>--}}
-{{--            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>--}}
-{{--        </div>--}}
-{{--    </form>--}}
 <main class="content">
     <div class="container-fluid p-0">
         <h1 class="h3 fw-bold">Feedback
